441ffe1c24eeae2ee1f0c3d8f56ecf6e06db3685
Alfred Ferrer Florensa <30677070+ffalfred@users.noreply.github.com>
2019-01-31 00:39:52 +0100

    Update phasing_trios.py

--- phasing_trios.py
+++ phasing_trios.py
@@ -12,8 +12,9 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Simple tool for phasing human PB-oocyte trios.')
     parser.add_argument("-i","--input_file", default=False, required=True, help="Input file in .txt format")
     parser.add_argument("-m","--min_snps", default=20, required=True, type=float,help="Amount of SNPs required to define a phase")
+    parser.add_argument("-o","--output_dir", default=False, required=True,help="Directory where the files are saved")
     args = parser.parse_args()
 
-    end=phasing_class.phasing_class_tool(args.input_file,args.min_snps)
+    end=phasing_class.phasing_class_tool(args.input_file,args.min_snps,args.output_dir)
     end.phasing() 
 
